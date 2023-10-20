299ee289c16c4f7237570bd99ccf763f0d163bab
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-20 17:07:40 -0500

    react router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Root from "./pages/Root.jsx";
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        }
+      ],
+    },
+  ]);
+
   return (
-    <div>
-      <p>Hello World!!!</p>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
