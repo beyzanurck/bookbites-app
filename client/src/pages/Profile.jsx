414da28874418bbdb26b916998f799a39d8e5bbc
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-22 09:35:47 -0500

    gets user's name from auth0 for profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Profile() {
+  
+  const { user } = useAuth0();
+
   return (
     <div>
-      <p>Profile Page</p>
+      <p> {user.name}'s Page </p>
     </div>
   )
 }
