637c56a38503b59355819389c93a05fe13637d43
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-22 09:45:48 -0500

    adds react-bootstrap cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import BookCard from '../components/BookCard'
 
 export default function Home() {
   return (
     <div>
         <h1>Beyza's Homepage</h1>
+        <BookCard />
     </div>
   )
 }
