499f6ee64502c6c795ee760903f2f7920f320d61
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-22 09:59:34 -0500

    fetches all books

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BookCard from '../components/BookCard'
 
 export default function Home() {
+
+  const [books, setBooks] = useState([]);
+
+  async function getBooks() {
+
+    try {
+      const response = await fetch("http://localhost:1212");
+
+      const allBooks = await response.json();
+      setBooks(allBooks);
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <div>
         <h1>Beyza's Homepage</h1>
+        <p>{books[0].categories}</p>
         <BookCard />
     </div>
   )
