4adfa00138d85612f64619c65733fc5790727bfe
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-23 13:36:54 -0500

    gets the book info by id

--- client/src/pages/Book.jsx
+++ client/src/pages/Book.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
 
 export default function Book() {
+
+    const [book, setBook] = useState([]);
+
+    let { id } = useParams();
+
+    async function getBookById() {
+        try {
+
+            const response = await fetch(`http://localhost:1212/${id}`);
+
+            const theBook = await response.json()
+
+            setBook(theBook)
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getBookById();
+    }, []);
+
   return (
     <div>
       <p>Book Page</p>
+      <img src={book[0] && book[0].image_url}/>
     </div>
   )
 }
