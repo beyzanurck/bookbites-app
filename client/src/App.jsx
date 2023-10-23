361a6a554d0d0c9d3a9d310f835d46f57016ec34
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-23 11:46:23 -0500

    creates single book page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.jsx";
 import Root from "./pages/Root.jsx";
 import Profile from "./pages/Profile.jsx";
 import About from "./pages/About.jsx";
+import Book from "./pages/Book.jsx";
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -24,6 +25,10 @@ function App() {
         {
           path: 'profile',
           element: <Profile />,
+        },
+        {
+          path: 'book',
+          element: <Book />,
         }
       ],
     },
