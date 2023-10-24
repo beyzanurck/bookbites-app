98e23edf5470df2509127c2a1cc367f6ef7e781f
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-24 15:06:15 -0500

    creates a dropdown menu on the book page

--- client/src/pages/Book.jsx
+++ client/src/pages/Book.jsx
@@ -33,11 +33,19 @@ export default function Book() {
 
       <div className='top'>
 
-        <img src={book[0] && book[0].image_url}/>
-        <MdFavorite size={32} style={{ color: 'red' }} />
-        <MdFavoriteBorder size={32}/>
+        <div className='book-img'>
+            <img src={book[0] && book[0].image_url}/>
+            <MdFavorite size={32} style={{ color: 'red' }} />
+            <MdFavoriteBorder size={32}/>
 
+            <select>
+                <option value="read">Read</option>
+                <option value="to-read">To Read</option>
+                <option value="currently-reading">Currently Reading</option>
+            </select>
+        </div>
 
+    
         <div className='book-details'>
             <p>{book[0] && book[0].title}</p>
             <p>{book[0] && book[0].description}</p>
