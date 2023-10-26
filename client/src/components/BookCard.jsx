b727719670e23e00e22dd05e51c6175ccb91998d
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-26 06:05:28 -0500

    adds fav icon to the home page if the user logged in

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import {MdFavoriteBorder } from 'react-icons/md';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 export default function BookCard({title, author, img, category, id}) {
 
   const navigate = useNavigate();
+  const { isAuthenticated} = useAuth0();
+
 
   return (
+
     <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={img} onClick={() => {navigate(`/book/${id}`)}} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                  <strong>Author:</strong> {author} <br />
-                  <strong>Category:</strong> {category}
-                </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+      <Card.Img variant="top" src={img} onClick={() => {navigate(`/book/${id}`)}} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            <strong>Author:</strong> {author} <br />
+            <strong>Category:</strong> {category}
+          </Card.Text>
+
+          {
+            isAuthenticated && <MdFavoriteBorder size={32} style={{ color: 'red' }}/>
+          }
+          
         </Card.Body>
     </Card>
+
   )
 }
