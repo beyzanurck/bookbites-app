7d073a0004979ba9620db96c082d29a033a8e320
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-11-06 12:36:14 -0600

    opens the note popup from the button on the profile page

--- client/src/components/NotePopup.jsx
+++ client/src/components/NotePopup.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import TextArea from './TextArea'
 
-export default function NotePopup() {
+export default function NotePopup({show, onClose}) {
+
+    const [action, setAction] = useState({
+        "isNotePrivate" : false,
+        "note" : ""
+    })
+
+    function handleChange (event) {
+
+        const { value, name } = event.target;
+        setAction((prevValue) => ({ ...prevValue, [name]: value }));
+    }
+
+    function handleSubmit(event) {
+
+        event.preventDefault();
+    }
+
+
   return (
-    <div>
-      
-    </div>
+    <Modal show={show} onHide={onClose} size="lg">
+
+        <Modal.Header closeButton>
+            <Modal.Title>Add New Note</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+            <form className='add-note'>
+
+            
+
+                <TextArea 
+                    placeholder={"Note"} 
+                    name = {"note"} 
+                    value = {action.note} 
+                    onChange={handleChange}
+                />
+
+            </form>
+                
+        </Modal.Body>
+
+        <Modal.Footer>
+
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Save</Button>
+
+            <Button variant="secondary" onClick={onClose}>
+                Close
+            </Button>
+
+        </Modal.Footer>
+
+    </Modal>
   )
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
+import NotePopup from '../components/NotePopup';
 
 export default function Profile() {
   
   const { user } = useAuth0();
+  const [show, setShow] = useState(false)
 
   return (
     <div>
       <p> {user && user.name}'s Page </p>
+
+      <button onClick={() => {setShow(true)}}>Add Note</button>
+
+      {
+            show && 
+            <NotePopup 
+                show = {show}
+                onClose={() => {setShow(false)}}
+            />
+        }
+
+
     </div>
   )
 }
