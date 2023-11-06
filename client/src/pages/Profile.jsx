df71f7117693fe76da36d51956829ee25c1bf97e
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-11-06 16:36:46 -0600

    creates GET fetch call to retrieve datas from 3 tables

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,38 @@
-import {React, useState} from 'react'
+import {React, useEffect, useState} from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 import NotePopup from '../components/NotePopup';
 
 export default function Profile() {
   
-  const { user } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
   const [show, setShow] = useState(false)
+  const [allActions, setAllActions] = useState([])
+
+
+  //gets all infos from 3 tables
+  async function getUserAllActions(id) {
+
+    try {
+      const response = await fetch(`/api/profile/${id}`);
+
+      const allActions = await response.json();
+      setAllActions(allActions);
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  useEffect(() => {
+    if(isAuthenticated) {
+      getUserAllActions(user.sub);
+    }
+  }, [isAuthenticated, user]); 
+
+  
+  useEffect(() => {
+    console.log(allActions);
+  }, [allActions]);
 
   return (
     <div>
